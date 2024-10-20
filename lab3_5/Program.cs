4ebc687ce2dcd897ce0ee151038fe7ca57a21f42
PartySoonXd <vladbelom2005@gmail.com>
2024-10-20 20:35:37 +0700

    Commit

--- lab3_5/Program.cs
+++ lab3_5/Program.cs
@@ -4,21 +4,59 @@ namespace lab3_5
 {
     internal class Program
     {
+        static int[] addItemToArray(ref int[] myArray, int item)
+        {
+            int[] newArray = new int[myArray.Length + 1];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                newArray[i] = myArray[i];
+            }
+
+            newArray[myArray.Length] = item;
+            return newArray;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Введите 0 для остановки");
             Console.Write("Введите число, которое хотите добавить в массив: ");
             int currentNum = Convert.ToInt32(Console.ReadLine());
 
-            int[] array = new int[1];
-            while (currentNum != 0)
+            int[] myArray = { currentNum };
+
+            while (true)
             {
-                array[array.Length - 1] = currentNum;
+                Console.Clear();
+
+                Console.WriteLine("Введите 0 для остановки");
+                Console.WriteLine($"Текущий массив: [{string.Join(", ", myArray)}]");
+                Console.Write("Введите число, которое хотите добавить в массив: ");
+
                 currentNum = Convert.ToInt32(Console.ReadLine());
+
+                if (currentNum == 0)
+                {
+                    break;
+                }
+                myArray = addItemToArray(ref myArray, currentNum);
             }
 
-            string str = string.Join(" ", array);
-            Console.WriteLine(str);
+            int maxValue = 0;
+            for (int i = 0; i < myArray.Length - 1; i++)
+            {
+                if (myArray[i] > myArray[i + 1] && maxValue < myArray[i])
+                {
+                    maxValue = myArray[i];
+                } else if (myArray[i] < myArray[i + 1] && maxValue < myArray[i + 1])
+                {
+                    maxValue = myArray[i + 1];
+                }
+            }
+
+            Console.WriteLine($"Максимальное число в массиве - {maxValue}");
+            myArray = addItemToArray(ref myArray, maxValue);
+            Console.WriteLine($"Финальный массив: [{string.Join(", ", myArray)}]");
+            Console.ReadLine();
         }
     }
 }
---
+++ lab3_6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int radius = 4;
+            int[,] points = { { 1, 5 }, { 2, 4 } };
+            int counter = 0;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                //double range = Math.Sqrt(points[i][);
+            }
+        }
+    }
+}
